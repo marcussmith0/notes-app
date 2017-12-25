63ccb3d8e7d1f3c6152402673539c8cf29f0f422
Marcus Smith <marcussmith0711@gmail.com>
2017-12-25 16:06:34 -0600

    changed something

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("you mom");
+    res.send("your mom");
 });
 
 const PORT = process.env.PORT || 5000;
